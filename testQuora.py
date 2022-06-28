7e55cb6c07c8012d0923b484640f579e1d5f382e
Pawanesh <55928870+Pawaneshmishra@users.noreply.github.com>
2022-06-28 12:04:53 +0530

    Update testQuora.py

--- testQuora.py
+++ testQuora.py
@@ -1,3 +1,5 @@
+#self.link, self.questions, self.answers have their respective results in them.
+
 from selenium import webdriver 
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
